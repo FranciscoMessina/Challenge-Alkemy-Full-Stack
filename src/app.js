4ba117a5738ef29b576b2aab43ba0558b1327d42
Francisco Messina <francisco@messina.ws>
2021-09-29 21:42:19 -0300

    Agregado de estructura de carpetas y archivos base

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-const app = require("express")();
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static("public"));
+
+const routes = require("./routes/routes");
+app.use("/api", routes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+	console.log(`Servidor corriendo en el puerto ${port}`);
+});
---
+++ src/controllers/controller.js
@@ -0,0 +1,7 @@
+module.exports = {
+	add: (req, res) => {
+		res.json({
+			test: "valor",
+		});
+	},
+};
